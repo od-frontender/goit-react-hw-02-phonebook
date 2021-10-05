12c77f96234803977588cc91253d7a2ee154be56
od-frontender <od.frontender@gmail.com>
2021-10-05 14:32:02 +0300

    project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <h1>Hello world</h1>;
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
 }
 
 export default App;
